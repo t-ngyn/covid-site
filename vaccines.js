f2d399f80ac7be218f5bfd6cfc783b42cdb72797
Tommy Nguyen <flex.innovations.corp@gmail.com>
2023-12-10 21:23:38 -0500

    first commit

---
+++ vaccines.js
@@ -0,0 +1,19 @@
+const VACCINES = [{"make":"AstraZeneca", "countries": 177},
+{"make":"Pfizer/BioNTech", "countries": 104},
+{"make":"Moderna", "countries": 54},
+{"make":"Johnson&Johnson", "countries": 25},
+{"make":"Sputnik V", "countries": 43},
+{"make":"Sinovac", "countries": 30},
+{"make":"Sinopharm/Beijing", "countries": 37},
+{"make":"Sinopharm/HayatVax", "countries": 1},
+{"make":"Sinopharm/Wuhan", "countries": 2},
+{"make":"Covaxin", "countries": 6},
+{"make":"BBIBP-CorV", "countries": 14},
+{"make":"Abdala", "countries": 1},
+{"make":"Soberana02", "countries": 1},
+{"make":"EpiVacCorona", "countries": 2},
+{"make":"QazVac", "countries": 1},
+{"make":"RBD-Dimer", "countries": 1},
+{"make":"CanSino", "countries": 4}];
+
+// const VACCINES = [{"salesperson":"Bob","sales":33},{"salesperson":"Robin","sales":12},{"salesperson":"Anne","sales":41},{"salesperson":"Mark","sales":16},{"salesperson":"Joe","sales":59},{"salesperson":"Eve","sales":38},{"salesperson":"Karen","sales":21},{"salesperson":"Kirsty","sales":25},{"salesperson":"Chris","sales":30},{"salesperson":"Lisa","sales":47},{"salesperson":"Tom","sales":5},{"salesperson":"Stacy","sales":20},{"salesperson":"Charles","sales":13},{"salesperson":"Mary","sales":29}];
